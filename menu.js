80d2ae6a76164ae09ec6135d5bcf45e3098813f1
patrickmaynard <patrickmaynard@users.noreply.github.com>
2021-02-19 10:12:14 +0100

    Make window resize work

--- menu.js
+++ menu.js
@@ -1,14 +1,34 @@
-$(function(){
-    if ($(window).height() > 500) {
-        $('#more').click(function(){
-            $('#menu').toggle();
-            if ($('#more')[0].innerHTML === '+') {
-                $('#more')[0].innerHTML = 'x'
-            } else {
-                $('#more')[0].innerHTML = '+'
-            }
+class Menu {
+    init = function () {
+        let that = this;
+        $(window).resize(function () {
+            that.handleSize();
         });
-        $('#menu').hide();
-        $('#more').show();
+        that.handleSize();
+    }
+
+    handleSize = function () {
+        let more = $('#more');
+        let menu = $('#menu');
+        if ($(window).height() > 500) {
+            more.click(function () {
+                $('#menu').toggle();
+                if (more[0].innerHTML === '+') {
+                    more[0].innerHTML = 'x'
+                } else {
+                    $('#more')[0].innerHTML = '+'
+                }
+            });
+            menu.hide();
+            more.show();
+        } else {
+            menu.show();
+            more.hide();
         }
+    }
+}
+
+$(function(){
+    let menu = new Menu;
+    menu.init();
 });
